d7b50644c212af77f54813fee3a81fb314d7ccb8
Ivo Vilar <ivo@grupovoz.dev.br>
2023-10-20 17:17:12 -0300

    adicionado validação de regras no formulario de registro

--- app/Livewire/Auth/Register.php
+++ app/Livewire/Auth/Register.php
@@ -5,14 +5,18 @@ namespace App\Livewire\Auth;
 
 use App\Models\User;
 use Illuminate\Contracts\View\View;
+use Livewire\Attributes\Rule;
 use Livewire\Component;
 
 class Register extends Component
 {
+    #[Rule(['required', 'max:255'])]
     public ?string $name = null;
+    #[Rule(['required','email', 'max:255', 'confirmed'])]
     public ?string $email = null;
     public ?string $email_confirmation = null;
 
+    #[Rule(['required'])]
     public ?string $password = null;
     public function render(): View
     {
@@ -21,6 +25,8 @@ class Register extends Component
 
     public function submit(): void
     {
+        $this->validate();
+
         User::query()
             ->create([
                 'name' => $this->name,

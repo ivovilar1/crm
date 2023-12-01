f9c226883940616f1e78d3a76d4096b283e1c899
Ivo Vilar <ivo@grupovoz.dev.br>
2023-12-01 16:14:02 -0300

    validando o token

--- tests/Feature/Auth/Password/ResetTest.php
+++ tests/Feature/Auth/Password/ResetTest.php
@@ -1,8 +1,33 @@
 <?php
 
+use App\Livewire\Auth\Password;
+use App\Models\User;
+use Illuminate\Auth\Notifications\ResetPassword;
+use Illuminate\Support\Facades\Notification;
+use Livewire\Livewire;
 use function Pest\Laravel\get;
 
-test('i need to have a route that will receive the token and the email that needs to be reset it', function () {
-    get(route('password.reset'))
-        ->assertOk();
+test('need to receive a valid token with a combination with the email and open the page', function () {
+    Notification::fake();
+
+    $user = User::factory()->create();
+
+    Livewire::test(Password\Recovery::class)
+        ->set('email', $user->email)
+        ->call('startPasswordRecovery');
+
+    Notification::assertSentTo(
+        $user,
+        ResetPassword::class,
+        function (ResetPassword $notification) {
+            get(route('password.reset') . '?token=' . $notification->token)
+                ->assertSuccessful();
+
+            get(route('password.reset') . '?token=any-token')
+                ->assertRedirect(route('login'));
+
+            return true;
+        }
+    );
+
 });

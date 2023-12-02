80c3725c441114fca973d71074024d7aa37578ed
Ivo Vilar <ivo_vilar@hotmail.com>
2023-12-02 10:26:32 -0300

    making sure that password is being updated

--- tests/Feature/Auth/Password/ResetTest.php
+++ tests/Feature/Auth/Password/ResetTest.php
@@ -3,9 +3,11 @@
 use App\Livewire\Auth\Password;
 use App\Models\User;
 use Illuminate\Auth\Notifications\ResetPassword;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Notification;
 use Livewire\Livewire;
 use function Pest\Laravel\get;
+use function PHPUnit\Framework\assertTrue;
 
 test('need to receive a valid token with a combination with the email and open the page', function () {
     Notification::fake();
@@ -26,6 +28,39 @@ test('need to receive a valid token with a combination with the email and open t
             get(route('password.reset') . '?token=any-token')
                 ->assertRedirect(route('login'));
 
+            return true;
+        }
+    );
+
+});
+
+test('test if is possible to reset the password with the given token', function () {
+    Notification::fake();
+
+    $user = User::factory()->create();
+
+    Livewire::test(Password\Recovery::class)
+        ->set('email', $user->email)
+        ->call('startPasswordRecovery');
+
+    Notification::assertSentTo(
+        $user,
+        ResetPassword::class,
+        function (ResetPassword $notification) use($user) {
+            Livewire::test(Password\Reset::class, [
+                'token' => $notification->token,
+                'email' => $user->email
+            ])
+                ->set('email_confirmation', $user->email)
+                ->set('password', 'new-password')
+                ->set('password_confirmation', 'new-password')
+                ->call('updatePassword')
+                ->assertHasNoErrors()
+                ->assertRedirect(route('dashboard'));
+
+            $user->refresh();
+
+            assertTrue(Hash::check('new-password', $user->password));
             return true;
         }
     );

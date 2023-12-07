e10285fa55fe2a783fc68b1d362cdb6f235d554c
Ivo Vilar <ivo_vilar@hotmail.com>
2023-12-07 20:49:05 -0300

    create a middleware to protect admin routes

--- tests/Feature/PermissionControlTest.php
+++ tests/Feature/PermissionControlTest.php
@@ -5,6 +5,7 @@ use App\Models\Permission;
 use App\Models\User;
 use Database\Seeders\PermissionSeeder;
 use Database\Seeders\UsersSeeder;
+use function Pest\Laravel\actingAs;
 use function Pest\Laravel\assertDatabaseHas;
 use function Pest\Laravel\seed;
 
@@ -50,4 +51,13 @@ test('seed with an admin user', function () {
         'user_id' => User::first()?->id,
         'permission_id' => Permission::where('key','=','be an admin')->first()?->id
     ]);
+});
+
+it('should block the access to an admin page if the user does not have the permission to be an admin', function () {
+
+    $user = User::factory()->create();
+
+    actingAs($user)
+        ->get(route('admin.dashboard'))
+        ->assertForbidden();
 });

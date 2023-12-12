153993e861231d03f73e31ecf07c052f48e5162f
Ivo Vilar <ivo_vilar@hotmail.com>
2023-12-12 19:47:14 -0300

    let's create a livewire component to list all users in the page

--- tests/Feature/Admin/UserManagement/ListUsersTest.php
+++ tests/Feature/Admin/UserManagement/ListUsersTest.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Livewire\Admin;
 use App\Models\User;
+use Livewire\Livewire;
 use function Pest\Laravel\actingAs;
 use function Pest\Laravel\get;
 
@@ -16,4 +18,22 @@ test('making sure that the route is protected by the permission BE_AN_ADMIN', fu
     actingAs(User::factory()->create());
 
     get(route('admin.users'))->assertForbidden();
+});
+
+it("let's create a livewire component to list all users in the page", function () {
+
+
+    $users = User::factory()->count(10)->create();
+
+    $livewire = Livewire::test(Admin\Users\Index::class);
+    $livewire->assertSet('users', function ($users) {
+                expect($users)
+                    ->toBeInstanceOf(\Illuminate\Pagination\LengthAwarePaginator::class)
+                    ->toHaveCount(10);
+
+                return true;
+            });
+    foreach ($users as $user) {
+        $livewire->assertSee($user->name);
+    }
 });

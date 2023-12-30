75e04ace57809f0235507f2b8aef10dda4d9379f
Ivo Vilar <ivo_vilar@hotmail.com>
2023-12-30 16:37:55 -0300

    installing laravel auditing

--- config/app.php
+++ config/app.php
@@ -159,7 +159,7 @@ return [
         /*
          * Package Service Providers...
          */
-
+        OwenIt\Auditing\AuditingServiceProvider::class,
         /*
          * Application Service Providers...
          */

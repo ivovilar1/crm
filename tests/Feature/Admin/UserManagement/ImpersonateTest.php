f5a13a2994a57fd1414af8d99cda62e3331c4bd7
Ivo Vilar <ivo_vilar@hotmail.com>
2023-12-31 11:52:38 -0300

    blocking user to impersonate yourself

--- tests/Feature/Admin/UserManagement/ImpersonateTest.php
+++ tests/Feature/Admin/UserManagement/ImpersonateTest.php
@@ -95,3 +95,14 @@ it('should have the correct permission to impersonate someone', function () {
         ->call('impersonate', $user->id)
         ->assertRedirect();
 });
+
+
+it('should not be possible impersonate myself', function () {
+
+    $admin = User::factory()->admin()->create();
+
+    actingAs($admin);
+
+    Livewire::test(Impersonate::class)
+        ->call('impersonate', $admin->id);
+})->throws(Exception::class);

0a01b87613e6e64f3f40bc4cd7df7e6234b5e8b9
Ivo Vilar <ivo@grupovoz.dev.br>
2024-03-05 14:16:20 -0300

    update an opportunity

--- app/Livewire/Opportunities/Update.php
+++ app/Livewire/Opportunities/Update.php
@@ -2,12 +2,42 @@
 
 namespace App\Livewire\Opportunities;
 
+use App\Models\Opportunity;
+use Illuminate\Contracts\View\View;
+use Livewire\Attributes\On;
 use Livewire\Component;
+use Mary\Traits\Toast;
 
 class Update extends Component
 {
-    public function render()
+    use Toast;
+
+    public Form $form;
+
+    public bool $modal = false;
+
+    public function render(): View
     {
         return view('livewire.opportunities.update');
+    }
+
+    #[On('opportunity::update')]
+    public function load(int $id): void
+    {
+        $opportunity = Opportunity::find($id);
+        $this->form->setOpportunity($opportunity);
+
+        $this->form->resetErrorBag();
+        $this->modal = true;
+    }
+
+    public function save(): void
+    {
+        $this->form->update();
+
+        $this->modal = false;
+        $this->dispatch('opportunity::reload')->to('opportunities.index');
+
+        $this->success('Opportunity updated!');
     }
 }

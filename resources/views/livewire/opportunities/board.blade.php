1a6ddd260d861a7d0782fcc98bf50cdc6f661429
Ivo Vilar <ivo@grupovoz.dev.br>
2024-03-27 17:20:11 -0300

    board layout

---
+++ resources/views/livewire/opportunities/board.blade.php
@@ -0,0 +1,26 @@
+<div class="p-2 grid grid-cols-3 gap-4 h-full">
+    <div class="bg-base-200 p-2 rounded-md">
+        <x-header title="Open" subtitle="Total 2 opportunities of 10" size="text-lg" class="px-2 pb-0 mb-2"
+                  separator progress-indicator/>
+        <div class="space-y-3 p-2">
+            <x-card>Opp1</x-card>
+            <x-card>Opp2</x-card>
+        </div>
+    </div>
+    <div class="bg-base-200 p-2 rounded-md">
+        <x-header title="Won" subtitle="Total 2 opportunities of 10" size="text-lg" class="px-2 pb-0 mb-2"
+                  separator progress-indicator/>
+        <div class="space-y-3 p-2">
+            <x-card>Opp1</x-card>
+            <x-card>Opp2</x-card>
+        </div>
+    </div>
+    <div class="bg-base-200 p-2 rounded-md">
+        <x-header title="Lost" subtitle="Total 2 opportunities of 10" size="text-lg" class="px-2 pb-0 mb-2"
+                  separator progress-indicator/>
+        <div class="space-y-3 p-2">
+            <x-card>Opp1</x-card>
+            <x-card>Opp2</x-card>
+        </div>
+    </div>
+</div>
--- resources/views/livewire/welcome.blade.php
+++ resources/views/livewire/welcome.blade.php
@@ -1,3 +1,3 @@
-<div class="text-red-4oo bg-red-700">
-    Welcome
+<div class="p-2 h-dvh">
+    <livewire:opportunities.board/>
 </div>

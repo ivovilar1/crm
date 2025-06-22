a2da85b7141c864ff755df8bc8e769c661b742ad
Ivo <ivo_vilar@hotmail.com>
2025-06-22 15:18:45 -0300

    Add customer tab navigation and related components
    - Introduced tab navigation for customer details, allowing users to switch between opportunities, tasks, and notes.
    - Created new Livewire components for tasks, notes, and opportunities.
    - Updated the customer show route to accept an optional tab parameter.
    - Enhanced the customer show Blade view to incorporate the new tab structure.

--- resources/views/livewire/customers/show.blade.php
+++ resources/views/livewire/customers/show.blade.php
@@ -5,7 +5,7 @@
         </x-slot:title>
     </x-header>
 
-    <div class="grid grid-cols-3 ">
+    <div class="grid grid-cols-3 gap-4">
         <div class="bg-base-200 rounded-md p-4 space-y-2 text-base gap-4 flex flex-col">
             <div class="space-y-1">
                 <x-info.title>Personal Info</x-info.title>
@@ -45,7 +45,21 @@
                 <x-info.data title="Updated At"> {{ $customer->updated_at->diffForHumans() }} </x-info.data>
             </div>
         </div>
-        <div class="bg-green-300 col-span-2">
-        </div>
+        <div class="bg-base-200 rounded-md text-base col-span-2">
+            <div class="py-2 bg-base-100 rounded-t-md w-full space-x-4 px-4">
+                <x-ui.tab :href="route('customers.show', [$customer, 'opportunities'])">
+                    Opportunities
+                </x-ui.tab>
+                <x-ui.tab :href="route('customers.show', [$customer, 'tasks'])">
+                    Tasks
+                </x-ui.tab>
+                <x-ui.tab :href="route('customers.show', [$customer, 'notes'])">
+                    Notes
+                </x-ui.tab>
+            </div>
+
+            <div class="">
+                @livewire("customers.$tab.index", ["customer" => $customer])
+            </div>
     </div>
 </div>

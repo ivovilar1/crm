c040f93a5fb2de0dc1c4cf19aa12d25721c1c3ec
Ivo <ivo_vilar@hotmail.com>
2025-09-30 20:36:57 -0300

    Enhance task editing functionality by adding user assignment feature
    - Introduced a new `selectedUser` property in the Livewire component to allow task assignment to users.
    - Updated validation rules to include user assignment and modified the save method to handle changes in assigned users.
    - Added a user selection dropdown in the Blade view for selecting the user assigned to the task.
    - Improved the display of assigned user information in the task list for better visibility.

--- app/Livewire/Customers/Tasks/Edit.php
+++ app/Livewire/Customers/Tasks/Edit.php
@@ -2,9 +2,11 @@
 
 namespace App\Livewire\Customers\Tasks;
 
-use App\Models\Task;
+use App\Models\{Task, User};
 use Illuminate\Contracts\View\View;
-use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\{Builder, Collection};
+use Illuminate\Validation\Rule;
+use Livewire\Attributes\Computed;
 use Livewire\Component;
 use Mary\Traits\Toast;
 
@@ -14,13 +16,21 @@ class Edit extends Component
 
     public Task $task;
 
+    public mixed $selectedUser = null;
+
     public bool $editing = false;
 
     public function rules(): array
     {
         return [
-            'task.title' => ['required', 'string', 'max:255'],
+            'task.title'   => ['required', 'string', 'max:255'],
+            'selectedUser' => ['nullable', Rule::exists('users', 'id')],
         ];
+    }
+
+    public function mount(): void
+    {
+        $this->selectedUser = $this->task->assigned_to;
     }
 
     public function render(): View
@@ -28,13 +38,29 @@ class Edit extends Component
         return view('livewire.customers.tasks.edit');
     }
 
+    #[Computed]
+    public function users(): Collection
+    {
+        return User::all();
+    }
+
     public function save(): void
     {
         $this->validate();
+
+        if ($this->selectedUser) {
+            $this->task->assigned_to = $this->selectedUser;
+        }
+
         $this->task->save();
+
+        if ($this->task->wasChanged('assigned_to')) {
+            $this->task->load('assignedTo');
+        }
 
         $this->info(__('Task updated successfully!'));
         $this->reset('editing');
+
     }
 
     public function toggleCheck(string $status): void
--- resources/views/livewire/customers/tasks/edit.blade.php
+++ resources/views/livewire/customers/tasks/edit.blade.php
@@ -6,6 +6,12 @@
                     wire:model="task.title"
                     class="input-xs input-ghost "
                 />
+                <x-select
+                    icon="o-user"
+                    :options="$this->users"
+                    wire:model="selectedUser"
+                    class="select-xs"
+                />
             </div>
             <div class="flex items-center gap-2">
                 <x-button type="button" wire:click="$set('editing', 0)" class="btn-xs btn-ghost">{{ __('Cancel') }}</x-button>
@@ -13,15 +19,13 @@
             </div>
         </form>
     @else
-        <div>
+        <div class="flex gap-2 items-center">
             <button wire:sortable.handle title="{{ __('Drag to reorder') }}" class="cursor-grab">
                 <x-icon name="o-queue-list" class="w-4 h-4 -mt-px opacity-30"/>
             </button>
             <input id="task-{{ $task->id }}" wire:click="toggleCheck('done')" type="checkbox" value="1" @if($task->done_at) checked @endif />
             <label for="task-{{ $task->id }}">{{ $task->title }}</label>
-            <select>
-                <option>assigned to: {{ $task->assignedTo?->name }}</option>
-            </select>
+            <div class="text-xs italic opacity-30">assigned to: {{ $task->assignedTo?->name }}</div>
         </div>
         <div class="flex items-start gap-2">
             <button title="{{ __('Edit Task') }}" class="cursor-pointer" wire:click="$set('editing', true)">
